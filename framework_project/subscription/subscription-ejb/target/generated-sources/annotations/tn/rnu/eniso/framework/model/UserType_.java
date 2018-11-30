c97016927d968f5dccb8ffc8cc9ea28885cda6ec
ISMAIL <ismmekni@gmail.com>
2018-11-30 22:05:49 +0100

    my project has been added

---
+++ framework_project/subscription/subscription-ejb/target/generated-sources/annotations/tn/rnu/eniso/framework/model/UserType_.java
@@ -0,0 +1,16 @@
+package tn.rnu.eniso.framework.model;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value = "org.hibernate.jpamodelgen.JPAMetaModelEntityProcessor")
+@StaticMetamodel(UserType.class)
+public abstract class UserType_ {
+
+	public static volatile SingularAttribute<UserType, String> code;
+	public static volatile SingularAttribute<UserType, String> name;
+	public static volatile SingularAttribute<UserType, Integer> id;
+
+}
+
